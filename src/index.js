1a9d254e708fff4fabf66fb43c13e9c5e2f0ab6a
Livelyoneweek <wlvvwhs@naver.com>
2023-11-26 11:41:08 +0900

    feat: 정적파일 서빙 add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const port = 4000;
 
@@ -7,6 +7,8 @@ app.get('/', (req,res) => {
     res.send('Hello, world!!');
 })
 
+console.log(path.join(__dirname, '../uploads'));
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 app.listen(4000, () => {
     console.log(`${port}번 서버 실행`)
