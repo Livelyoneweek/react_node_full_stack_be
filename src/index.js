b78d36d0a3296adb61cfb65798f881345e4e66fc
Livelyoneweek <wlvvwhs@naver.com>
2023-11-26 12:53:25 +0900

    feat: mongoose , env 연결

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const port = 4000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('연결완료')
+})
+.catch(err => {
+    console.error(err)
+});
+
 
 app.get('/', (req,res) => {
     res.send('Hello, world!!');
+})
+
+app.post('/', (req,res) => {
+    console.log(req.body);
+    res.json(req.body);
 })
 
 console.log(path.join(__dirname, '../uploads'));
